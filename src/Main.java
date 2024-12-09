183a9f25c1c36fa526050029acdb038b63ddb105
Elenilton <eleniltonfns3@gmail.com>
2024-12-08 23:31:36 -0300

    Sistema de Média e verificação de aprovação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
+/**
+ *  Classe main - Coletar as notas e armazenar em um array, validando cada nota na
+ * hora que for inserida, onde caso não seja uma nota válida, o sistema irá esperar
+ * que seja inserida uma nota válida, após as notas coletadas será realizado o calcúlo
+ * da média e por fim informa o estado de aprovação do aluno com base na sua média.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("\n -=-=-=- Sistema de Média -=-=-=- \n");
+
+        Media media = new Media();
+
+        double[] notas = new double[4]; // - Arreio para armazenar as notas.
+
+        // - Loop para coletar as 4 notas.
+        for (int i = 0; i < notas.length; i++) {
+            boolean notaValida;
+
+            do { // Loop para permitir que a nota apenas seja armazenada caso a nota esteja válida.
+                System.out.print(" - Informe a " + (i + 1) + "° nota: ");
+                double nota = entrada.nextDouble();
+
+                notaValida = media.validarNota(nota); // - Valida a nota.
+
+                if (notaValida) {
+                    notas[i] = nota; // - Armazena a nota no arreio.
+                } else {
+                    System.out.println("\n - Nota invalida! Tente novamente!");
+                }
+            }while (!notaValida);
+        }
+
+        // - Calcular média.
+        double mediaNota = media.fazerMediaNota(notas[0],notas[1],notas[2],notas[3]);
+
+        // - Verificar aprovação
+        boolean aprovacao = media.verificarAprovacao(mediaNota);
+
+        // - Exibição dos resultados.
+        System.out.println("\n -=-=- Resultados: ");
+
+        System.out.printf("\n - Média: %.2f\n", mediaNota); // - Imprime a média
+        System.out.print(" - Estado de Aprovação: " + (aprovacao ? "Aprovado" : "Reprovado") + "\n"); // - Imprime se o aluno foi aprovado ou não
     }
 }
--- src/Media.java
+++ src/Media.java
@@ -1,11 +1,11 @@
 /**
- * Classe para calcular a média e verificar o estado de aprovação da média.
- *  Fornece métodos para calcular a média aritmética das 4 notas informadas
- *  e verifica se o aluno que tirou essas notas foi aprovado ou não.
+ *  Classe para calcular a média e verificar o estado de aprovação da média.
+ *  Fornece métodos para calcular a média aritmética das 4 notas informadas,
+ * e verifica se o aluno que tirou essas notas foi aprovado ou não.
  */
 public class Media {
     /**
-     *  Método para calcular a média aritmética das quatro notas fornecidas.
+     * - Método para calcular a média aritmética das quatro notas fornecidas.
      *
      * @param nota1 Primeira nota informada
      * @param nota2 Segunda nota informada
@@ -13,17 +13,27 @@ public class Media {
      * @param nota4 Quarta nota informada
      * @return Retorna o resultado da divisão entre a soma de todas as notas por quatro.
      */
-    public double media (double nota1, double nota2, double nota3, double nota4){
+    public double fazerMediaNota(double nota1, double nota2, double nota3, double nota4){
         return ((nota1+nota2+nota3+nota4)/4);
     }
 
     /**
-     *  Método para validar o estado de aprovação do aluno, tendo como média mínima exigida 6,0.
+     * - Método para validar o estado de aprovação do aluno, tendo como média mínima exigida 6,0.
      *
      * @param media Média aritmética já calculada
-     * @return Retorna {@code true} caso o aluno seja aprovado e {@code false} caso não seja
+     * @return Retorna {@code true} caso o aluno seja aprovado e {@code false} caso o aluno não seja
      */
     public boolean verificarAprovacao (double media){
         return media >= 6.0;
+    }
+
+    /**
+     * - Método para validar se a nota é válida de 0 a 10.
+     *
+     * @param nota Nota informada pelo úsuario
+     * @return Retorna {@code true} caso a nota seja válida {@code false} caso ela seja inválida
+     */
+    public boolean validarNota (double nota){
+        return nota >= 0.0 && nota <= 10.0;
     }
 }
